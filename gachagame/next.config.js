a673686f212e852f04f22ff62ab8cff1ffe17176
Lorenzo <henriquewillians@usp.br>
2024-10-31 20:39:34 -0300

    testando cors

---
+++ gachagame/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Isso define os cabeçalhos CORS para todas as rotas API
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Accept, Accept-Version, Content-Length, Content-Type, Authorization' },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig;
